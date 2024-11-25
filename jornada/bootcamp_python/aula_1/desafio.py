3f41d78b83eff45a9507cdf687d4580335fcc7b5
LeoFaleiros <leonardo.engquimico@gmail.com>
2024-11-25 13:40:13 -0300

    fix: terminando desafio aula 1

--- jornada/bootcamp_python/aula_1/desafio.py
+++ jornada/bootcamp_python/aula_1/desafio.py
@@ -1,21 +1,25 @@
-CONSTANTE_BONUS = 1000
+CONSTANTE_BONUS = 1000      #Boa prática usar constantes em maiúsculas
 
 # 1) Solicita ao usuário que digite seu nome
-nome_usuario = input("Digite o seu nome: ")
+nome_usuario = input("Digite seu nome: ")
 
 # 2) Solicita ao usuário que digite o valor do seu salário
 # Converte a entrada para um número de ponto flutuante
-salario_usuario = float(input("Digite o seu salario: "))
+salario_usuario = float(input("Digite o valor do seu salário: "))
 
 # 3) Solicita ao usuário que digite o valor do bônus recebido
 # Converte a entrada para um número de ponto flutuante
-bonus_usuario = float(input("Digite o seu bonus: "))
+bonus_usuario = float(input("Digite o bônus recebido: "))
 
 # 4) Calcule o valor do bônus final
+bonus_final = CONSTANTE_BONUS + salario_usuario * bonus_usuario
 
-valor_do_bonus = CONSTANTE_BONUS + salario_usuario * bonus_usuario
-
-# 5) Imprime a mensagem personalizada incluindo o nome do usuário e o valor do bonus
-print(f"O usuario {nome_usuario} possui o bonus de {valor_do_bonus}")
+# 5) Imprime a mensagem personalizada incluindo o nome do usuário, salário e bônus
+print(f"O usuário {nome_usuario} possui o bonus no valor de {bonus_final}.")
 
 # Bônus: Quantos bugs e riscos você consegue identificar nesse programa?
+""""
+Não há tratativas para evitar erros de entrada de dados, como por exemplo:
+- usuário pode não digitar o nome, ou preencher com valores inválidos
+- usuário pode digitar o salário e bonus por extenso
+"""
