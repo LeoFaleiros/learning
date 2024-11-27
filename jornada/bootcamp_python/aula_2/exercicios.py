4f08e8d86752396032f2210a5983499305052ced
LeoFaleiros <leonardo.engquimico@gmail.com>
2024-11-27 13:53:48 -0300

    upd exercicio 12

--- jornada/bootcamp_python/aula_2/exercicios.py
+++ jornada/bootcamp_python/aula_2/exercicios.py
@@ -103,14 +103,21 @@
 
 # #### Strings (`str`)
 # 11. Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
-def converter_maiusculas(texto):
-    return texto.upper()
+# def converter_maiusculas(texto):
+#     return texto.upper()
 
 
-texto = input("Digite um texto: ")
-print(converter_maiusculas(texto))
+# texto = input("Digite um texto: ")
+# print(converter_maiusculas(texto))
 
 # 12. Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+def converter_minusculas(texto):
+    return texto.lower()
+
+texto = input("Digite o seu nome seu nome completo: ")
+print(converter_minusculas(texto))
+
+
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
